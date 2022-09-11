1d35b5e32ae8c385f2c1758675e835f0ddecd54d
VitoB00 <95907691+VitoB00@users.noreply.github.com>
2022-09-11 19:05:47 +0200

    Add files via upload

---
+++ Project-COVID19_firststep.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  6 14:39:14 2022
+
+@author: vitob
+"""
+import numpy as np
+import pandas
+from matplotlib import pyplot as plt
+
+
+def get_data_reg(url_reg, regione):
+    date = []
+    tot_positivi = []
+    source = pandas.read_csv(url_reg)
+    lista = source.values
+
+    for line in lista:
+        if str(line[3]) == regione:
+            date.append(line[0])
+            tot_positivi.append(line[10])
+
+    return date, tot_positivi
+
+
+def main():
+    date, tot_positivi = get_data_reg("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv", "Friuli Venezia Giulia")
+  
+    x = np.arange(0,len(date))
+    y = np.array(tot_positivi)
+    # disegniamo il grafico
+    plt.plot(x,y)
+    plt.show()
+
+
+main()
+
